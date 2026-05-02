0765ebe0301fa90c9d503a1d6f1f8a623bdd73ed
Ignacio Fernandez <ignaciofernandez565@gmail.com>
2026-05-02 19:44:20 -0300

    Ejercicio 1

---
+++ Dsw2026Ej8/scripts/ProductHelper.aux.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public partial class ProductHelper
+{
+	public string FormatearMoneda(decimal price)
+	{
+		return price.ToString("C2");
+    }
+
+    
+}
---
+++ Dsw2026Ej8/scripts/ProductHelper.main.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public partial class ProductHelper
+{
+	public string ObtenerEtiquetaProducto(long code, string description, decimal price)
+	{
+        string priceFormat = FormatearMoneda(price);
+        return $"[ {code} ], {description} - {priceFormat}";
+    }
+}
