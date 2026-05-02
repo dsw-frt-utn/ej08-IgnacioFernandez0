fffe71847f82b858731632b4ce0c9ab3cbcbd4e5
Ignacio Fernandez <ignaciofernandez565@gmail.com>
2026-05-02 20:31:42 -0300

    Ejercicio3

---
+++ Dsw2026Ej8/scripts/Problema3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Product
+{
+	private long _code;
+    private string _description;
+    private decimal _price;
+
+	public void setDescription(string description)
+	{
+		_description = description;
+	}
+	public string getDescription() 
+	{
+		return _description;	
+	}
+}
+
+public class Problema3
+{
+	public string CompararCopias(int originalValue, Product product)
+	{
+		int copyValue = originalValue;
+		copyValue++;
+		Product copyProduct = product;
+		copyProduct.setDescription("Esta es la descripcion del producto copiado modificada");
+		return $"{originalValue}-{copyValue}-{copyProduct.getDescription}";
+    }
+}
