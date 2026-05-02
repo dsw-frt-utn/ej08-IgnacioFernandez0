b5150a6ee8ea8038a95897c5829c6a55711fe302
Ignacio Fernandez <ignaciofernandez565@gmail.com>
2026-05-02 20:57:17 -0300

    Ejercicio5

---
+++ Dsw2026Ej8/scripts/Problema5.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class Sale
+{
+    private decimal _importeBase;
+	public virtual decimal CalcularTotal()
+	{
+        return _importeBase;
+	}
+
+    public void setImporteBase(decimal importeBase)
+    {
+        _importeBase = importeBase;
+    }
+    public decimal getImporteBase()
+    {
+        return _importeBase;
+    }
+}
+
+public class RetailSale : Sale
+{
+    public override decimal CalcularTotal()
+    {
+        return getImporteBase();
+    }
+}
+
+public class WholesaleSale : Sale
+{
+    public override decimal CalcularTotal()
+    {
+        return getImporteBase() * 0.9m;
+    }
+}
+
+public class Problema5
+{
+	public decimal ObtenerImporteFinal(Sale sale)
+	{
+        sale.CalcularTotal();
+	}
+}
