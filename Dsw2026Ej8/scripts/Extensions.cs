2d894d04b8cc8be73864c4543f69d8ada9884287
Ignacio Fernandez <ignaciofernandez565@gmail.com>
2026-05-02 21:19:38 -0300

    Ejercicio6

---
+++ Dsw2026Ej8/scripts/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public static class Extensions
+{
+	public static string ToProductCode(this string code)
+	{
+		if (code.IsNullOrWhiteSpace(code))
+		{
+			return "SIN-CODIGO";
+		}
+        return code.Trim().ToUpper().Replace(" ", "-");
+	}
+}
